ae67cbae9ee21f64c0acc7a817e55826c7524e04
Timothy-Wangwe <wangwetymothy@gmail.com>
2020-07-17 09:03:07 +0300

    Map urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import accounts, register, login, profile
 
+
+app_name = 'accounts'
 urlpatterns = [
     path('', accounts, name='accounts'),
     path('register/', register, name='register'),
