cb4f01a2d57a3d6032952b8f6114aa54b7fa503c
Timothy-Wangwe <wangwetymothy@gmail.com>
2020-07-18 01:49:00 +0300

    Set default views ie. they do nothing but work

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+
+def accounts(request):
+	"""Displays options for registration/login on landing"""
+	return render(request, 'accounts/accounts.html')
+
+def register(request):
+	"""Registration of new accounts"""
+	return render(request, 'accounts/register.html')
+
+def login(request):
+	"""Logs into existing accounts"""
+	return render(request, 'accounts/login.html')
+
+def profile(request):
+	"""Display info about certain account"""
+	return HttpResponse('View profile here...')
