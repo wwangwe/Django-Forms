be769bb0fc1e36bf8e4c0d22b856c154754a971a
Timothy-Wangwe <wangwetymothy@gmail.com>
2020-07-18 09:10:57 +0300

    Hide 'SECRET_KEY' and other secrets in 'secrets.py' and 'gitignore' them so they are not commited

--- dforms/settings.py
+++ dforms/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-
+from .secrets import secret_key
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'y#p-vvo!dd!%ld$m_$duym8q2^hy5*n*pd9$8m+utgo%iv9wmc'
+SECRET_KEY = secret_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
